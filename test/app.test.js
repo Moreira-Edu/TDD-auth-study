030bbe9ec011556e8c5de8f7a69c306a858600f8
Moreira-Edu <ml.ri7u@gmail.com>
2022-12-27 10:56:24 -0300

    build: dev env setup

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+import supertest from "supertest";
+import { app } from '../src/app.js'
+
+const agent = supertest(app)
+describe('app behavior', () => {
+  test('should respond at root', async () => {
+    const { status } = await agent.get('/')
+    expect(status).toBe(200);
+  })
+
+})
