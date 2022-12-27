912cd0f39c3be1d3f75c5efdc628e0ed5ad9e7ef
Moreira-Edu <ml.ri7u@gmail.com>
2022-12-27 11:32:35 -0300

    test: user test get/post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ import express from "express";
 
 export const app = express();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.status(200).send();
+});
+
+
+app.get('/users', (req, res) => {
+  const users = [{ name: 'John Doe', email: 'john@email.com' }]
+  res.status(200).json(users)
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body)
 });
---
+++ test/user.test.js
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+import { app } from "../src/app.js";
+
+
+
+const agent = supertest(app)
+describe('user route behavior', () => {
+  test('should list all users', async () => {
+    const { body, status } = await agent.get('/users');
+    expect(status).toBe(200);
+    expect(body[0]).toHaveProperty('name', 'John Doe');
+  })
+
+  test('should register a user successfully', async () => {
+    const { status, body } = await agent.post('/users').send({ name: 'Walter Sap', email: 'walter@email.com' })
+    expect(status).toBe(201);
+    expect(body.name).toBe('Walter Sap');
+
+  })
+
+})
