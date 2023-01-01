07a82e1923a16172ef6456f158ed6e6e4b2c3860
Moreira-Edu <ml.ri7u@gmail.com>
2023-01-01 19:25:14 -0300

    test: user email validation

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -5,6 +5,7 @@ const agent = supertest(app);
 describe("user route behavior", () => {
   test("should list all users", async () => {
     const { body, status } = await agent.get("/users");
+
     expect(status).toBe(200);
     expect(body.length).toBeGreaterThan(0);
   });
@@ -13,6 +14,7 @@ describe("user route behavior", () => {
     const email = `${Date.now()}@email.com`;
     const { status, body } = await agent.post("/users")
       .send({ name: "Walter Sap", email, password: "word@123" });
+
     expect(status).toBe(201);
     expect(body.name).toBe("Walter Sap");
   });
@@ -23,7 +25,19 @@ describe("user route behavior", () => {
       password: "1234oi",
     };
     const { status, body } = await agent.post("/users").send(user);
+
     expect(status).toBe(400);
     expect(body.error).toBe("Nome é um atributo obrigatório");
+  });
+
+  test("should not register a user without email", async () => {
+    const user = {
+      name: "John Doe",
+      password: "122345",
+    };
+    const { status, body } = await agent.post("users").send(user);
+
+    expect(status).toBe(400);
+    expect(body.error).toBe("E-mail é um atributo obrigatório");
   });
 });
